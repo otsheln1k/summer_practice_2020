f2106c066e3f6835ea172d82814891f0b4c16c14
Leonid Parfentiev <leo.parfentiev@gmail.com>
2020-07-06 13:20:28 +0300

    fix duplicate edges when using ShuffleGraphEdgeGenerator
    - Also added a test for that

--- src/main/java/summer_practice_2020/purple/graphgen/ShuffleGraphEdgeGenerator.java
+++ src/main/java/summer_practice_2020/purple/graphgen/ShuffleGraphEdgeGenerator.java
@@ -8,18 +8,18 @@ import java.util.Random;
 import summer_practice_2020.purple.IGraph;
 
 public class ShuffleGraphEdgeGenerator implements GraphEdgeGenerator {
-	
+
 	private final int edgesCount;
 	private final Random rng = new Random();
 
 	public ShuffleGraphEdgeGenerator(int edgesCount) {
 		this.edgesCount = edgesCount;
 	}
-	
+
 	private class EdgeCandidate {
 		private final IGraph.Node a;
 		private final IGraph.Node b;
-		
+
 		public EdgeCandidate(IGraph.Node a, IGraph.Node b) {
 			this.a = a;
 			this.b = b;
@@ -38,23 +38,25 @@ public class ShuffleGraphEdgeGenerator implements GraphEdgeGenerator {
 	public void generateEdges(IGraph g) {
 		List<IGraph.Node> nodes = new ArrayList<>();
 		g.getNodes().forEach(nodes::add);
-		
+
 		List<EdgeCandidate> possibleEdges = new ArrayList<>();
 		for (int i = 0; i < nodes.size(); i++) {
 			IGraph.Node ni = nodes.get(i);
-			for (int j = 0; j < nodes.size(); j++) {
+			for (int j = i+1; j < nodes.size(); j++) {
 				IGraph.Node nj = nodes.get(j);
 				if (g.getEdgeBetween(ni, nj) != null) {
 					continue;
 				}
-				
+
 				possibleEdges.add(new EdgeCandidate(ni, nj));
 			}
 		}
-		
+
 		Collections.shuffle(possibleEdges, rng);
-		
-		for (int i = 0; i < edgesCount; i++) {
+
+		int realEdgesCount = Math.min(edgesCount, possibleEdges.size());
+
+		for (int i = 0; i < realEdgesCount; i++) {
 			EdgeCandidate e = possibleEdges.get(i);
 			g.addEdge(e.getFirstNode(), e.getSecondNode());
 		}
