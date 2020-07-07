da115602f94d88a589cbf0683ecf94f6f7ecd0d5
DaniilYR <daniil8303@gmail.com>
2020-07-08 01:56:30 +0300

    Change Boruvkadisconnected graphs 08/07/2020 02:00

--- src/main/java/summer_practice_2020/purple/Boruvka.java
+++ src/main/java/summer_practice_2020/purple/Boruvka.java
@@ -6,6 +6,7 @@ public class Boruvka {
 
     private IGraph g;
     private HashMap<IGraph.Node, Integer> componentMap = new HashMap<IGraph.Node, Integer>();
+    private HashMap<IGraph.Node, String> visitedMap = new HashMap<IGraph.Node, String>();
     private int amountCompanent = 1;
     private Iterable<IGraph.Node> nodes;
     private List<IGraph.Edge> list = new ArrayList<IGraph.Edge>();
@@ -22,30 +23,40 @@ public class Boruvka {
         return Double.compare(e1.getWeight(), e2.getWeight());
     }
 
-    /*static void dfs(String v, Map<String, Map<String, Integer>> vertexMap, HashMap<String, String> visitedMap){
+    private void dfs(IGraph.Node v){
         visitedMap.put(v, "visited");
-        for(Map.Entry<String, Integer> now: vertexMap.get(v).entrySet() ){
-            if(now.getValue() > 0 && visitedMap.get(now.getKey()).equals("not_visited")) {
-                dfs(now.getKey(), vertexMap, visitedMap);
+        for(IGraph.Edge now: g.getEdgesFrom(v)) {
+            if (v.equals(now.firstNode())) {
+                if (visitedMap.get(now.secondNode()).equals("not_visited")) {
+                    System.out.println("Second = " + now.secondNode().getTitle());
+                    System.out.println();
+                    dfs(now.secondNode());
+                }
+            }
+            else {
+                if (visitedMap.get(now.firstNode()).equals("not_visited")) {
+                    System.out.println("First = " + now.firstNode().getTitle());
+                    System.out.println();
+                    dfs(now.firstNode());
+                }
             }
         }
     }
 
-    private int component(Graph g){
+    private int component(){
         int result = 0;
-        vertexMap = g.getVertexMap();
-        for(Map.Entry<String, Map<String, Integer>> it: vertexMap.entrySet()){
-            visitedMap.put(it.getKey(), "not_visited");
+        for(IGraph.Node it: g.getNodes()){
+            visitedMap.put(it, "not_visited");
         }
 
-        for(Map.Entry<String, Map<String, Integer>> it: vertexMap.entrySet()){
-            if(visitedMap.get(it.getKey()).equals("not_visited")){
-                dfs(it.getKey(), vertexMap, visitedMap);
+        for(IGraph.Node it:g.getNodes()){
+            if(visitedMap.get(it).equals("not_visited")){
+                dfs(it);
                 result++;
             }
         }
         return result;
-    }*/
+    }
 
     private boolean hasNext_step() {
         if (SnapShot.size() < g.nodesCount() - amountCompanent) {
@@ -55,62 +66,21 @@ public class Boruvka {
         }
     }
 
-    private void next_step(int mark, IGraph.Edge edge) { //Graph snapshot,
-        //Выбрать мин ребро
-        //System.out.println("ШАГ");
-        /*Iterable<IGraph.Edge> edges = g.getEdges();
-        double min = 2000000;
-        boolean flag_add_edge = false;
-        IGraph.Edge edge = null;
-        for (IGraph.Edge e : edges) {
-            if (e.getWeight() < min) {
-                min = e.getWeight();
-                edge = e;
-            }
-        }*/
-        //boolean flag_add_edge = false;
-
-        //System.out.println("Текущее ребро " + edge.firstNode().getTitle() + " " + edge.secondNode().getTitle() + " " + edge.getWeight());
-
-        //Проверить на группу
-        //System.out.println("Найденое ребро " + edge.firstNode().getTitle() + " " + edge.secondNode().getTitle() + " " + edge.getWeight());
+    private void next_step(int mark, IGraph.Edge edge) {
         if (componentMap.get(edge.firstNode()).equals(0) && componentMap.get(edge.secondNode()).equals(0)) {
             componentMap.put(edge.firstNode(), mark);
             componentMap.put(edge.secondNode(), mark);
-            //Добавить в snapshot
-            //snapshot.addEdge(edge.firstNode(), edge.secondNode()).setWeight(edge.getWeight());
             SnapShot.add(edge);
-            //flag_add_edge = true;
-            //System.out.println("Добавили ребро: " + edge.firstNode().getTitle() + " " + edge.secondNode().getTitle() + " " + edge.getWeight());
-            ///edge.setWeight(2000000);
-            //System.out.println("Обе вершины ребра имели метки 0");
-            //System.out.println("Метки теперь: " + componentMap.get(edge.firstNode()) + " " + componentMap.get(edge.secondNode()));
         } else {
-            //System.out.println("Первый else");
-            //System.out.println(componentMap.get(edge.firstNode()) + " " + componentMap.get(edge.secondNode()));
-            //System.out.println(componentMap.get(edge.firstNode()) * componentMap.get(edge.secondNode()));
             if (componentMap.get(edge.firstNode()) * componentMap.get(edge.secondNode()) == 0) {
                 mark = Math.max(componentMap.get(edge.firstNode()), componentMap.get(edge.secondNode()));
-                //System.out.println("Метки вершин: " + componentMap.get(edge.firstNode()) + " " + componentMap.get(edge.secondNode()));
-                //System.out.println("Ставим метку " + mark);
                 componentMap.put(edge.firstNode(), mark);
                 componentMap.put(edge.secondNode(), mark);
-                //Добавить в snapshot
-                //snapshot.addEdge(edge.firstNode(), edge.secondNode()).setWeight(edge.getWeight());
                 SnapShot.add(edge);
-                //flag_add_edge = true;
-                //System.out.println("Добавили ребро: " + edge.firstNode().getTitle() + " " + edge.secondNode().getTitle() + " " + edge.getWeight());
-                ///edge.setWeight(2000000);
-                //System.out.println("Одна из вершин ребра имела метку 0");
-                //System.out.println("Метки теперь: " + componentMap.get(edge.firstNode()) + " " + componentMap.get(edge.secondNode()));
             } else {
-                //System.out.println("Второй else");
-                //System.out.println(componentMap.get(edge.firstNode()).equals(componentMap.get(edge.secondNode())));
                 if (componentMap.get(edge.firstNode()) != componentMap.get(edge.secondNode())) {
                     mark = Math.min(componentMap.get(edge.firstNode()), componentMap.get(edge.secondNode()));
                     int not_mark = Math.max(componentMap.get(edge.firstNode()), componentMap.get(edge.secondNode()));
-                    //System.out.println("Метки вершин: " + componentMap.get(edge.firstNode()) + " " + componentMap.get(edge.secondNode()));
-                    //System.out.println("Ставим метку " + mark);
                     for(Map.Entry<IGraph.Node, Integer> e: componentMap.entrySet()){
                         if(e.getValue().equals(not_mark)){
                             componentMap.put(e.getKey(), mark);
@@ -118,23 +88,10 @@ public class Boruvka {
                     }
                     componentMap.put(edge.firstNode(), mark);
                     componentMap.put(edge.secondNode(), mark);
-                    //Добавить в snapshot
-                    //snapshot.addEdge(edge.firstNode(), edge.secondNode()).setWeight(edge.getWeight());
                     SnapShot.add(edge);
-                    //flag_add_edge = true;
-                    //System.out.println("Добавили ребро: " + edge.firstNode().getTitle() + " " + edge.secondNode().getTitle() + " " + edge.getWeight());
-                    ///edge.setWeight(2000000);
-                    //System.out.println("Ни одна из вершин ребра имела метку 0");
-                    //System.out.println("Метки теперь: " + componentMap.get(edge.firstNode()) + " " + componentMap.get(edge.secondNode()));
                 }
             }
         }
-        /*if (flag_add_edge) {
-            return snapshot;
-        } else {
-            edge.setWeight(2000000);
-            return snapshot;
-        }*/
     }
 
     public void boruvka() {
@@ -142,15 +99,12 @@ public class Boruvka {
         Iterable<IGraph.Node> nodes = g.getNodes();
         Graph snapshot = new Graph();
         g.getEdges().forEach(list::add);
-        //System.out.println("LIST");
         Collections.sort(list, this::MyCompare);
-        /*for(IGraph.Edge le: list){
-            System.out.println(le.firstNode().getTitle() + " " + le.secondNode().getTitle() + " " + le.getWeight());
-        }
-        System.out.println();*/
 
         int mark = 1;
         int num = 0;
+
+        amountCompanent = component();
 
         for (IGraph.Node n : nodes) {
             IGraph.Node nnode = snapshot.addNode();
@@ -165,9 +119,8 @@ public class Boruvka {
             blist.add(BoruvkaSnapshot.fromMapAndSet(componentMap, SnapShot));
             mark++;
             num++;
-
         }
-        //SnapShot.add(snapshot);
+
     }
 
     public Iterable<IGraph.Edge> getSnapShotSet(){
