98feef1d7815c6f679d3554a0dd5d8de995db412
Эльдар <abibulaev.eldar@gmail.com>
2020-07-12 01:27:19 +0300

    Improved step-by-step algorithm working. Also added step choosing. Current step is now marked in ListView.

--- src/main/java/summer_practice_2020/purple/Controller.java
+++ src/main/java/summer_practice_2020/purple/Controller.java
@@ -29,7 +29,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.HashSet;
 import java.util.LinkedList;
 
 public class Controller {
@@ -77,7 +76,7 @@ public class Controller {
         this.next.setDisable(true);
         this.previous.setDisable(true);
         this.stop.setDisable(true);
-        this.speed_control.setMin(0);
+        this.speed_control.setMin(15);
         this.speed_control.setMax(10);
         this.speed_control.setBlockIncrement(0.5);
 
@@ -137,9 +136,6 @@ public class Controller {
                 importError.setContentText("Ошибка при импортировании графа");
                 importError.showAndWait();
             }
-            this.renderer.setGraph(this.graphToWork);
-            this.renderer.setEdgeSet(new HashSet<>());
-            this.renderer.drawGraph();
         });
 
         exportGraph.setOnAction(e -> {
@@ -226,9 +222,6 @@ public class Controller {
             this.renderer.drawGraph();
             list.setItems(FXCollections.observableArrayList());
             this.isGraphBlocked = false;
-            this.stop.setDisable(true);
-            this.next.setDisable(true);
-            this.previous.setDisable(true);
             timeline.stop();
         });
 
